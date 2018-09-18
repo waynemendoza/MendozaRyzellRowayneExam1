074689645d741f52253140d779e859cad1093a4b
waynemendoza <2011027847@ust-ics.mygbiz.com>
2018-09-18 15:31:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wayne/practicalexam/PracExam.java
@@ -0,0 +1,13 @@
+package com.example.wayne.practicalexam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracExam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prac_exam);
+    }
+}
